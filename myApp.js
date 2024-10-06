b41c390f58bf6f98f1efa7de26d929cccaefe3b2
Kat Zongker <katrina@zongker.net>
2024-10-06 14:33:28 -0500

    updatesTwo

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 console.log("Hello World");
 
@@ -7,11 +8,28 @@ console.log("Hello World");
     res.send("Hello Express")
 }); */
 
+app.use(function (req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use("/public", express.static(__dirname + "/public"));
+
+/* app.get("/json", function (req, res) {
+    res.json({ "message": "Hello json" });
+}); */
+
+app.get("/json", function (req, res) {
+    if (process.env["MESSAGE_STYLE"] == "uppercase") {
+        res.json({ "message": "HELLO JSON" });
+    } else {
+        res.json({ "message": "Hello json" });
+    }
+});
 
 
 
