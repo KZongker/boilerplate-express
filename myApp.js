d086e66fddc68def3269b5e4a5294549fee74df6
Kat Zongker <katrina@zongker.net>
2024-10-06 14:00:30 -0500

    Updates

--- myApp.js
+++ myApp.js
@@ -3,10 +3,15 @@ let app = express();
 
 console.log("Hello World");
 
-app.get("/", function (req, res) {
+/* app.get("/", function (req, res) {
     res.send("Hello Express")
+}); */
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
